48fade5374ab2b66394fede8f69e847bcf35f7c9
Fitz <caojunkaiv@gmail.com>
2016-12-13 13:41:17 +0800

    修改定时器

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->exec('more /wwwroot/cjkbe/storage/logs/laravel.log >> /wwwroot/test.log')->cron('1 * * * * ');
+         $schedule->exec('more /wwwroot/cjkbe/storage/logs/laravel.log >> /wwwroot/test.log')->cron('*/1 * * * * ');
     }
 
     /**
